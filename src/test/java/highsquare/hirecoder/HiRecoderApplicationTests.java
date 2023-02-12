cb83ad677b97418097e34e41ccdf38b0f83af454
kangwook <rlarkddnr1686@naver.com>
2023-02-12 18:04:26 +0900

    <feat> 프로젝트 init

---
+++ src/test/java/highsquare/hirecoder/HiRecoderApplicationTests.java
@@ -0,0 +1,13 @@
+package highsquare.hirecoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiRecoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
