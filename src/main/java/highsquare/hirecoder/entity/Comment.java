70b4369437d28900472e086c3675da3e1f8d5b06
Kyungrim Park <pkl4693@gmail.com>
2023-02-15 19:12:04 +0900

    # 제목 작성 ## 제목 작성 후 공백을 한 줄 포함해야 함. ## 제목 첫 글자는 대문자로 작성, 마침표 사용 금지. ## 제목은 명령문으로 사용, 과거형은 사용하지 않음. ## 제목은 50글자로 제한 ## <타입> : <제목> 형식으로 작성 ## 타입 목록 #### feat  : 새로운 기능 추가 #### fix   : 버그 수정 #### docs  : 문서 추가 및 수정 #### test  : 테스트 코드 추가 #### clean : 코드 리팩토링 #### style : 코드 의미에 영향을 주지않는 변경 #### chore : 빌드 부분 혹은 패키지 매니저 수정 사항 #################### 제목 작성 #################### <feat> : <Comment 클래스>
    ####################
    # 본문 작성
    ## 본문 작성 후 공백을 한 줄 포함해야 함.
    ## 본문의 각 행은 72자 이하.
    ## 본문은 "왜"와 "무엇을" 위주로 작성
    ## 본문의 내용은 * 로 시작함.
    ############################### 본문 작성###############################
    
    ####################
    # Resolves 작성
    ## Resolves 작성 후 공백을 한 줄 포함해야 함.
    ## Resolves의 내용은 * 로 시작함.
    ## 해결한 이슈는 닫을 것.
    ## 이슈의 종료시
    ### '키워드 #N 이슈제목'
    ### 키워드 목록
    #### close   : 일반 개발 이슈
    #### fix     : 버그 fix 이슈
    #### resolve : 요청사항 이슈
    ########## Resolves 작성(생략 가능)##########

---
+++ src/main/java/highsquare/hirecoder/entity/Comment.java
@@ -0,0 +1,31 @@
+package highsquare.hirecoder.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Comment {
+
+    @Id @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedTime;
+
+    private int likeCount;
+
+}
