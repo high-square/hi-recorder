e00d64153ad3e60d9ad3c3a80dcbbcc993b837ed
kangwook <rlarkddnr1686@naver.com>
2023-02-21 21:43:01 +0900

    <feat> 게시글 수정,삭제 버튼 작성 및 좋아요수 update(비동기) 기능 작성
    * 게시글 작성자 본인일 경우 수정하기, 삭제하기 버튼 보이게 구현
    * 수정하기 버튼 클릭시 수정하기 폼으로 이동
    * 삭제하기 버튼 클릭시 해당 게시글 삭제 후 스터디 게시판으로 이동
    * 좋아요 클릭시 좋아요 이미지 변환 및 좋아요수 update하여 화면단에 변경 기능 구현

--- src/main/java/highsquare/hirecoder/web/controller/BoardController.java
+++ src/main/java/highsquare/hirecoder/web/controller/BoardController.java
@@ -1,6 +1,5 @@
 package highsquare.hirecoder.web.controller;
 
-import highsquare.hirecoder.domain.repository.LikeOnBoardRepository;
 import highsquare.hirecoder.domain.service.BoardService;
 import highsquare.hirecoder.domain.service.CommentService;
 import highsquare.hirecoder.domain.service.LikeOnBoardService;
@@ -13,10 +12,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -62,12 +63,33 @@ public class BoardController {
         return "boards/board";
     }
 
+    // 수정하기를 누르면 수정하기 폼 화면으로 가게 설정
+    @GetMapping("/{id}/edit")
+    public String editForm(@PathVariable("id") Long boardId, Model model) {
+        model.addAttribute("boardId", boardId);
+        return "boards/editForm";
+    }
+
+    // 삭제하기를 누르면 해당 게시글을 삭제하고 그 게시글이 속했던 스터디 게시글 페이지로 이동하게 했음
+    // 삭제 시 foreignKey 제약조건으로 인해 삭제가 안됨 -- 설정해야함
+    @GetMapping("/{id}/delete")
+    public String deleteBoard(@PathVariable("id") Long boardId, @RequestParam Long studyId, RedirectAttributes redirectAttributes) {
+        boardService.deleteBoard(boardId);
+        redirectAttributes.addAttribute("studyId", studyId);
+        return "redirect:/study/{studyId}/boards";
+    }
+
+
     @PostMapping("/like")
     @ResponseBody
-    public String likeProcess(@RequestParam(name="board_id") Long boardId,
+    public List<Object> likeProcess(@RequestParam(name="board_id") Long boardId,
                               @RequestParam(name="member_id") Long memberId) {
         LikeOnBoard likeOnBoard = likeOnBoardService.updateLike(boardId, memberId);
-        return String.valueOf(likeOnBoard.getLikeCheck());
+        Integer likeCnt = likeOnBoardService.countLikeCnt(boardId, memberId);
+        List<Object> data = new ArrayList<>();
+        data.add(String.valueOf(likeOnBoard.getLikeCheck()));
+        data.add(likeCnt);
+        return data;
     }
 
 }
