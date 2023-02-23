223b2ef4b5aea59f203ee091127ec6e56ed4d264
Kyungrim Park <pkl4693@gmail.com>
2023-02-23 15:46:26 +0900

    <feat> : study/myStudy에서 내 스터디 목록 조회

--- src/main/java/highsquare/hirecoder/domain/repository/StudyMemberRepository.java
+++ src/main/java/highsquare/hirecoder/domain/repository/StudyMemberRepository.java
@@ -1,11 +1,17 @@
 package highsquare.hirecoder.domain.repository;
 
 import highsquare.hirecoder.domain.repository.custom.StudyMemberRepositoryCustom;
-import highsquare.hirecoder.entity.Board;
-import highsquare.hirecoder.entity.Member;
+import highsquare.hirecoder.entity.Comment;
 import highsquare.hirecoder.entity.Study;
 import highsquare.hirecoder.entity.StudyMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudyMemberRepository extends JpaRepository<StudyMember, Long>, StudyMemberRepositoryCustom {
+
+    @Query("select sm.study from StudyMember sm where sm.member.id =:memberId")
+    List<Study> findAllStudyByMemberId(@Param("memberId") Long memberId);
 }
