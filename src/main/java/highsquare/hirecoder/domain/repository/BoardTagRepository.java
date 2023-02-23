9fe612b4d6baf2c0a436ae2c592bfda7576c5593
JaeDoo <fosong98@gmail.com>
2023-02-23 16:42:38 +0900

    <feat>: 게시글 생성 컨트롤러에 태그 저장 로직 추가
    close #44

---
+++ src/main/java/highsquare/hirecoder/domain/repository/BoardTagRepository.java
@@ -0,0 +1,7 @@
+package highsquare.hirecoder.domain.repository;
+
+import highsquare.hirecoder.entity.BoardTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardTagRepository extends JpaRepository<BoardTag, Long> {
+}
