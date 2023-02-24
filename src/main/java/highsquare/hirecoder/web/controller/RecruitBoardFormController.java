049acb1204f9e2e8cc5c31f520761d33392dd92f
JaeDoo <fosong98@gmail.com>
2023-02-24 14:07:15 +0900

    <feat>: RecruitBoardForm 획득(GET) 컨트롤러 로직 작성

--- src/main/java/highsquare/hirecoder/web/controller/RecruitBoardFormController.java
+++ src/main/java/highsquare/hirecoder/web/controller/RecruitBoardFormController.java
@@ -1,12 +1,16 @@
 package highsquare.hirecoder.web.controller;
 
+import highsquare.hirecoder.domain.service.StudyMemberService;
+import highsquare.hirecoder.entity.Study;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -14,9 +18,19 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/boards/recruit")
 public class RecruitBoardFormController {
 
+    private final StudyMemberService studyMemberService;
+
     @GetMapping("/create")
-    public void chooseStudy(HttpSession session) {
+    public String getRecruitBoardCreateForm(HttpSession session, Model model) {
 
         Long memberId = (Long) session.getAttribute("member_id");
+
+        List<Study> studies = studyMemberService.getAllMembersStudy(memberId);
+
+        if (studies.isEmpty()) {
+            model.addAttribute("unsociable", true);
+        }
+
+        return "form/recruitBoardCreateForm";
     }
 }
