45277d1f4a2c64695bf89ad62ca42d9cba353171
Kyungrim Park <pkl4693@gmail.com>
2023-02-25 17:09:53 +0900

    <feat> : findMyPosts 특정 스터디에서 내가 쓴 글 조회 service

--- src/main/java/highsquare/hirecoder/domain/repository/StudyMemberRepository.java
+++ src/main/java/highsquare/hirecoder/domain/repository/StudyMemberRepository.java
@@ -1,6 +1,7 @@
 package highsquare.hirecoder.domain.repository;
 
 import highsquare.hirecoder.domain.repository.custom.StudyMemberRepositoryCustom;
+import highsquare.hirecoder.entity.Board;
 import highsquare.hirecoder.entity.Comment;
 import highsquare.hirecoder.entity.Study;
 import highsquare.hirecoder.entity.StudyMember;
@@ -14,4 +15,7 @@ public interface StudyMemberRepository extends JpaRepository<StudyMember, Long>,
 
     @Query("select sm.study from StudyMember sm where sm.member.id =:memberId")
     List<Study> findAllStudyByMemberId(@Param("memberId") Long memberId);
+
+    @Query("select b from Board b where b.study.id =: studyId and b.member.id =:memberId")
+    List<Board> findAllBoardByStudyIdAndMemberId(@Param("studyId") Long studyId, @Param("memberId") Long memberId);
 }
