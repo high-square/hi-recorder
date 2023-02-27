22a934dd02c887fdf7ced086bdf1db7a504e96a7
JaeDoo <fosong98@gmail.com>
2023-02-27 21:48:50 +0900

    <fix>: JpaAuditing 설정 분리
    * JpaAuditing 애노테이션으로 발생하는 테스트 충돌 문제를 해결하기 위한 설정 분리

--- src/main/java/highsquare/hirecoder/HiRecoderApplication.java
+++ src/main/java/highsquare/hirecoder/HiRecoderApplication.java
@@ -2,9 +2,9 @@ package highsquare.hirecoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class HiRecoderApplication {
 
@@ -12,4 +12,8 @@ public class HiRecoderApplication {
 		SpringApplication.run(HiRecoderApplication.class, args);
 	}
 
+	@Configuration
+	@EnableJpaAuditing
+	public static class JpaAuditingConfiguration {
+	}
 }
--- src/test/java/highsquare/hirecoder/web/controller/RecruitBoardFormControllerTest.java
+++ src/test/java/highsquare/hirecoder/web/controller/RecruitBoardFormControllerTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(RecruitBoardFormController.class)
+@MockBean(JpaMetamodelMappingContext.class)
 class RecruitBoardFormControllerTest {
 
     @Autowired
