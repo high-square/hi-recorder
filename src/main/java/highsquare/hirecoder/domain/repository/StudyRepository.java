3758ff654ef3c3c7c152d20b229ec97820a736d2
JaeDoo <fosong98@gmail.com>
2023-02-27 21:02:33 +0900

    <feat>: 스터디 생성 폼 획득 로직

--- src/main/java/highsquare/hirecoder/domain/repository/StudyRepository.java
+++ src/main/java/highsquare/hirecoder/domain/repository/StudyRepository.java
@@ -4,5 +4,9 @@ import highsquare.hirecoder.entity.Board;
 import highsquare.hirecoder.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    public List<Study> findAllByManager_Id(Long managerId);
 }
--- src/main/java/highsquare/hirecoder/web/controller/RecruitBoardFormController.java
+++ src/main/java/highsquare/hirecoder/web/controller/RecruitBoardFormController.java
@@ -1,6 +1,7 @@
 package highsquare.hirecoder.web.controller;
 
 import highsquare.hirecoder.constant.SessionConstant;
+import highsquare.hirecoder.domain.repository.StudyRepository;
 import highsquare.hirecoder.domain.service.BoardService;
 import highsquare.hirecoder.domain.service.StudyMemberService;
 import highsquare.hirecoder.domain.service.TagService;
@@ -28,18 +29,24 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/boards/recruit")
 public class RecruitBoardFormController {
+    private static final int MAX_STUDY_COUNT = 5;
 
-    private final StudyMemberService studyMemberService;
-    private final BoardService boardService;
+    private final StudyRepository studyRepository;
     private final TagService tagService;
 
     @GetMapping("/create")
     public String getRecruitBoardCreateForm(HttpSession session, Model model) {
 
         // 테스트용 데이터
-        session.setAttribute(SessionConstant.MEMBER_ID, 1L);
+//        session.setAttribute(SessionConstant.MEMBER_ID, 1L);
 
         Long memberId = (Long) session.getAttribute(SessionConstant.MEMBER_ID);
+
+        List<Study> managedStudies = studyRepository.findAllByManager_Id(memberId);
+
+        if (managedStudies.size() >= MAX_STUDY_COUNT) {
+            model.addAttribute("max_study", true);
+        }
 
         model.addAttribute("studyCreationForm", new StudyCreationForm());
 
@@ -51,6 +58,8 @@ public class RecruitBoardFormController {
     public String postRecruitBoardCreateForm(@ModelAttribute BoardForm boardForm, BindingResult bindingResult, HttpSession session) {
 
         Long memberId = (Long) session.getAttribute(SessionConstant.MEMBER_ID);
+
+        // 스터디 생성 로직
 
 
 
