6213bebc71cc5861ebe5e004e609b41579a55037
JaeDoo <fosong98@gmail.com>
2023-03-20 17:33:07 +0900

    <feat> : 내 스터디 조회 API 추가

---
+++ src/main/java/highsquare/hirecoder/web/api/service/StudyApiService.java
@@ -0,0 +1,25 @@
+package highsquare.hirecoder.web.api.service;
+
+import highsquare.hirecoder.domain.repository.StudyRepository;
+import highsquare.hirecoder.dto.MyStudyResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyApiService {
+
+    private final StudyRepository studyRepository;
+
+    public List<MyStudyResponse> getMyStudies(Long memberId) {
+        return studyRepository.findAllByManager_Id(memberId)
+                .stream()
+                .map((study)->new MyStudyResponse(study.getId(), study.getName()))
+                .collect(Collectors.toList());
+    }
+}
