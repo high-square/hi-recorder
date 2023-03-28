44207beb848773e7231dc25f4e117eb3833ceef9
JaeDoo <fosong98@gmail.com>
2023-03-28 15:00:31 +0900

    <fix> : nav 내 스터디 목록 조회 로직 수정

--- src/main/java/highsquare/hirecoder/api/service/StudyApiService.java
+++ src/main/java/highsquare/hirecoder/api/service/StudyApiService.java
@@ -1,5 +1,6 @@
 package highsquare.hirecoder.api.service;
 
+import highsquare.hirecoder.domain.repository.StudyMemberRepository;
 import highsquare.hirecoder.domain.repository.StudyRepository;
 import highsquare.hirecoder.dto.MyStudyResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +15,10 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class StudyApiService {
 
-    private final StudyRepository studyRepository;
+    private final StudyMemberRepository studyMemberRepository;
 
     public List<MyStudyResponse> getMyStudies(Long memberId) {
-        return studyRepository.findAllByManager_Id(memberId)
+        return studyMemberRepository.findAllStudyByMemberId(memberId)
                 .stream()
                 .map((study)->new MyStudyResponse(study.getId(), study.getName()))
                 .collect(Collectors.toList());
