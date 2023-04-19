45879187f355b9eec4198427a77b1ae934bb8660
JaeDoo <fosong98@gmail.com>
2023-04-19 17:24:07 +0900

    <feat> : 로그인 페이지 검증 로직 작성

--- src/main/java/highsquare/hirecoder/web/form/LoginForm.java
+++ src/main/java/highsquare/hirecoder/web/form/LoginForm.java
@@ -12,11 +12,11 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class LoginForm {
 
-    @NotNull
-    @Size(min = 3, max = 50)
+    @NotNull(message = "아이디를 입력하세요.")
+    @Size(min = 3, max = 50, message = "아이디의 길이는 3~50 사이입니다.")
     private String username;
 
-    @NotNull
-    @Size(min = 3, max = 100)
+    @NotNull(message = "비밀번호를 입력하세요.")
+    @Size(min = 3, max = 100, message = "비밀번호의 길이는 3~100 사이입니다.")
     private String password;
 }
